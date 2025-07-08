8914f84cd1defa2033398b847e9ad3530536215a
Fedor <diorcula@duck.com>
2025-07-08 17:02:29 +0200

    working on customers tab

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from "next";
+import { lusitana } from "@/app/ui/fonts";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const allCustomers = await fetchFilteredCustomers("");
+
+  return (
+    <main>
+      <div>
+        <CustomersTable customers={allCustomers} />
+      </div>
+    </main>
+  );
 }
